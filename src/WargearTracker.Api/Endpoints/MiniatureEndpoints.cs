bd76ec4eef077a888fc5f697c83f4a3ed1b55da2
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-11 13:33:44 +0200

    feat(api): GET miniatures

---
+++ src/WargearTracker.Api/Endpoints/MiniatureEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WargearTracker.Data;
+
+namespace WargearTracker.Api.Endpoints;
+
+public static class MiniatureEndpoints
+{
+    public static void MapMiniatureEndpoints(this WebApplication app)
+    {
+        app.MapGet("/miniatures", async (WargearDbContext db) =>
+        {
+            return await db.Miniatures.ToListAsync();
+        });
+    }
+}
