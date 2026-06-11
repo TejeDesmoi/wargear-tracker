8acd3d5904313105bf312ce61a20f727a3007718
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-11 12:38:28 +0200

    feat(api): GET armies

---
+++ src/WargearTracker.Api/Endpoints/ArmyEnpoints.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WargearTracker.Data;
+
+namespace WargearTracker.Api.Endpoints;
+public static class ArmyEnpoints
+{
+    public static void MapArmyEndpoints(this WebApplication app)
+    {
+        app.MapGet("/armies", async (WargearDbContext db) =>
+        {
+            return await db.Armies.ToListAsync();
+        });
+    }
+}
