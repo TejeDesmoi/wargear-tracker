b0cf197a338130ad7ae6631d9724b4600a278a28
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-11 13:36:50 +0200

    feat(api): POST miniature

--- src/WargearTracker.Api/Endpoints/MiniatureEndpoints.cs
+++ src/WargearTracker.Api/Endpoints/MiniatureEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WargearTracker.Core;
 using WargearTracker.Data;
 
 namespace WargearTracker.Api.Endpoints;
@@ -10,6 +11,13 @@ public static class MiniatureEndpoints
         app.MapGet("/miniatures", async (WargearDbContext db) =>
         {
             return await db.Miniatures.ToListAsync();
+        });
+
+        app.MapPost("/miniatures", async (WargearDbContext db, Miniature miniature) =>
+        {
+            db.Miniatures.Add(miniature);
+            await db.SaveChangesAsync();
+            return Results.Created($"/miniatures/{miniature.Id}", miniature);
         });
     }
 }
