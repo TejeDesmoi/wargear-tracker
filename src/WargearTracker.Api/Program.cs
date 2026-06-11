c6f8b4757d93c6d9cc6a8bdc0bf853e6e4528dfa
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-11 12:49:33 +0200

    feat(api): add swagger and endpoint

--- src/WargearTracker.Api/Program.cs
+++ src/WargearTracker.Api/Program.cs
@@ -1,25 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using WargearTracker.Api.Endpoints;
 using WargearTracker.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<WargearDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-
+app.MapArmyEndpoints();
 app.Run();
-
