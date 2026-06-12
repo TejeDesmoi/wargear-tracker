d43d9267d1cb9071cfb00f6233cdc7f616b9b0d1
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-12 09:01:44 +0200

    feat(core): add User entity

---
+++ src/WargearTracker.Core/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargearTracker.Core;
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
