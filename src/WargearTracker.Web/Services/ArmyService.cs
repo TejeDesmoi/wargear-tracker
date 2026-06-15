11294d8fd11b8f9a32649548cffa592165c73f24
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-15 09:43:54 +0200

    fix(web): delete unused namespace in army service

--- src/WargearTracker.Web/Services/ArmyService.cs
+++ src/WargearTracker.Web/Services/ArmyService.cs
@@ -1,7 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Microsoft.JSInterop;
-using WargearTracker.Core;
 using WargearTracker.Web.Models;
 
 namespace WargearTracker.Web.Services;
