6919d2602262e33a88d32a909ce8b0b61f261e80
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-15 09:41:34 +0200

    feat(web): add Army Serices

---
+++ src/WargearTracker.Web/Services/ArmyService.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Microsoft.JSInterop;
+using WargearTracker.Core;
+using WargearTracker.Web.Models;
+
+namespace WargearTracker.Web.Services;
+
+public class ArmyService
+{
+    private readonly HttpClient _http;
+    private readonly IJSRuntime _js;
+    private readonly AuthService _auth;
+
+    public ArmyService(HttpClient http, IJSRuntime js)
+    {
+        _http = http;
+        _js = js;
+        _auth = new AuthService(http, js);
+    }
+
+    public async Task<List<ArmyModel>> GetArmiesAsync()
+    {
+        var token = await _auth.GetTokenAsync();
+        if (string.IsNullOrEmpty(token))
+            throw new UnauthorizedAccessException("User is not authenticated.");
+
+        _http.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        return await _http.GetFromJsonAsync<List<ArmyModel>>("armies") ?? new List<ArmyModel>();
+    }
+
+    public async Task CreateArmyAsync(string name, string faction, string game)
+    {
+        var token = await _auth.GetTokenAsync();
+        if (string.IsNullOrEmpty(token))
+            throw new UnauthorizedAccessException("User is not authenticated.");
+        _http.DefaultRequestHeaders.Authorization =
+    new AuthenticationHeaderValue("Bearer", token);
+
+        await _http.PostAsJsonAsync("armies", new
+        {
+            name,
+            faction,
+            game
+        });
+    }
+
+    public async Task DeleteArmyAsync(Guid id)
+    {
+        var token = await _auth.GetTokenAsync();
+        if (string.IsNullOrEmpty(token))
+            throw new UnauthorizedAccessException("User is not authenticated.");
+        _http.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        await _http.DeleteAsync($"armies/{id}");
+    }
+
+    public async Task<ArmyModel?> GetArmyAsync(Guid id)
+    {
+        var token = await _auth.GetTokenAsync();
+        if (string.IsNullOrEmpty(token))
+            throw new UnauthorizedAccessException("User is not authenticated.");
+        _http.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        return await _http.GetFromJsonAsync<ArmyModel>($"armies/{id}");
+    }
+}
