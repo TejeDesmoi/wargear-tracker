d8efd951eaa97a954a124f95a3fd54f13375f53e
AlbertNagami <Alberto.Tejero@Nagami.Design>
2026-06-17 10:44:02 +0200

    fix(api): Add dto for paintstatus and migrate new colum name

---
+++ src/WargearTracker.Data/Migrations/20260617083806_RenameStatusToPaintStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WargearTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameStatusToPaintStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Miniatures",
+                newName: "PaintStatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaintStatus",
+                table: "Miniatures",
+                newName: "Status");
+        }
+    }
+}
